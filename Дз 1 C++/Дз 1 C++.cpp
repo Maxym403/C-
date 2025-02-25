95c3a28498436bb22f0ecafe5b3f23c5ad8d6616
Maxym403 <maxzalyga@gmail.com>
2025-02-25 11:49:41 +0200

    Add files via upload

---
+++ Дз 1 C++/Дз 1 C++.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << "\t\t\t\t\t\tRESUME\n";
+    cout << "\tMy name is Maxym Zaluha, I am 15 years old\n";
+    cout << "\tIT specialist with experience in software development and system administration.\n";
+    cout << "\tPassionate about solving technical issues and improving system performance.\n";
+    cout << "\t\tWORK EXPERIENCE\n";
+    cout << "Provided technical support and troubleshooting\n";
+    cout << "Developed and maintained web applications.\n";
+    cout << "Managed system administration and security.\n";
+    cout << "\t\tSKILLS\n";
+    cout << "Programming: C++, Python, HTML, JavaScript\n";
+    cout << "Database Management: MySQL, PostgreSQL\n";
+    cout << "Troubleshooting & Technical Support\n";
+    cout << "\t\tLANGUAGES\n";
+    cout << "English\n";
+    cout << "Ukrainian\n";
+
+
+}
+
+
